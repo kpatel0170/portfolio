c0fcb3dbbfc223fd9f2785f0dedaf5e23d544df9
kpatel0170 <kartikpatel0170@gmail.com>
2024-02-02 01:13:48 -0700

    feat: update layout and routing

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar/NavBar";
-import Project from "./pages/Project/Project";
-import Education from "./pages/Education/Education";
-import Experience from "./pages/Experience/Experience";
-import HomePage from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
-import Resume from "./components/Resume/Resume";
+const Project = React.lazy(() => import("./pages/Project/Project"));
+const Education = React.lazy(() => import("./pages/Education/Education"));
+const Experience = React.lazy(() => import("./pages/Experience/Experience"));
+const HomePage = React.lazy(() => import("./pages/Home/Home"));
+const Resume = React.lazy(() => import("./components/Resume/Resume"));
+
 
 const routes = [
   { path: "/", element: <HomePage /> },
   { path: "/projects", element: <Project /> },
   { path: "/education", element: <Education /> },
   { path: "/experience", element: <Experience /> },
-  { path: "/resume", element: <Resume /> }, 
+  { path: "/resume", element: <Resume /> },
+  { path: "*", element: <HomePage /> }, 
 ];
+
+
+const Spinner = () => (
+<div class="flex items-center justify-center h-screen">
+    <div class="relative">
+        <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+        <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+        </div>
+    </div>
+</div>
+);
+
 
 const Layout = ({ children }) => (
   <div className="flex flex-col min-h-screen">
@@ -26,17 +40,20 @@ const Layout = ({ children }) => (
 );
 
 function App() {
+  const routeElements = routes.map(({ path, element }, index) => (
+    <Route key={index} path={path} element={element} />
+  ));
+
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {routes.map(({ path, element }, index) => (
-            <Route key={index} path={path} element={<Layout>{element}</Layout>} />
-          ))}
-        </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Layout>
+          <Routes>{routeElements}</Routes>
+        </Layout>
       </Suspense>
     </Router>
   );
 }
+
 
 export default App;
